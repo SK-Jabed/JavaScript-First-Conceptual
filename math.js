20f704872cdf873f014396184d70f2cf5ba7a988
Sheikh Jabed <jabedbd2295@gmail.com>
2024-08-31 22:39:25 +0600

    Math Js File Updated

--- math.js
+++ math.js
@@ -1,5 +1,75 @@
+// Addition Operation
+
 var x = 5;
 var y = 10;
 var z = 8.5;
 
 console.log(x + y + z);
+
+var x = 5;
+var y = 10;
+var z = 8.5;
+var total = x + y + z;
+
+console.log(total);
+
+
+// Addition Operation
+
+var x = 5;
+var y = 10;
+var z = 8.5;
+
+console.log(x + y + z);
+
+var x = 5;
+var y = 10;
+var z = 8.5;
+var total = x + y + z;
+
+console.log(total);
+
+
+// Addition Operation
+
+var x = 5;
+var y = 10;
+var z = 8.5;
+
+console.log(x + y + z);
+
+var x = 5;
+var y = 10;
+var z = 8.5;
+var total = x + y + z;
+
+console.log(total);
+
+
+// Addition Operation
+
+var x = 5;
+var y = 10;
+var z = 8.5;
+
+console.log(x + y + z);
+
+var x = 5;
+var y = 10;
+var z = 8.5;
+var total = x + y + z;
+
+console.log(total);
+
+
+// Addition Operation
+
+var x = 5;
+var y = 10;
+var z = 8.5;
+var add = y + z;
+var sub = y - x;
+var multi = x * y;
+var divide = y / x;
+
+console.log(total);
