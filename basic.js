240990cd1e3e9d79cca38d8ee3f5eee488e6ea15
Sheikh Jabed <jabedbd2295@gmail.com>
2024-08-31 22:24:21 +0600

    First JavaScript Conceptual repo added

---
+++ basic.js
@@ -0,0 +1,74 @@
+// console.log(5); // Number Type
+// console.log('SKJ'); // String Type
+// console.log(true); // Boolean Type
+
+
+// var me = "SKJ";
+// console.log(me);
+// var me = 18;
+// console.log(me);
+// var me = "5 Feet 6 Inch";
+// console.log(me);
+
+// var me = "SKJ";
+// var age = 18;
+// console.log(me);
+// console.log(age);
+
+
+// Data Types: number, string, boolean, undefined, null, NaN
+
+// var x = 69;
+// console.log(typeof x);
+// var x = "69";
+// console.log(typeof x);
+// var x = true;
+// console.log(x);
+
+// var x;
+// console.log(typeof x);
+// var x = null;
+// console.log(typeof x);
+// var x = NaN;
+// console.log(typeof x);
+
+
+// truthy values ==> true, "hello", number
+// falsy values ==> false, "", 0
+
+// var x = 5;
+// console.log(!!x);
+// var x = 0;
+// console.log(!!x);
+// var x = "hello";
+// console.log(!!x);
+// var x = "";
+// console.log(!!x);
+
+
+// variable naming convention
+
+// Possible
+// var age;
+// var my_age; // snake_case
+// var myAge; // camelCase
+// var MyAge; // PascalCase
+// var age1;
+
+// Not possible
+// var 98;
+// var 2age;
+// var my-age;
+// var @age;
+// var class; [Can not use any reserved keyword]
+
+// Good Practice
+// var height;
+// var myHeight; // camel case
+// var my_height; // snake case
+
+var age = "my age";
+var Age = "your age";
+console.log(age);
+console.log(Age);
+
