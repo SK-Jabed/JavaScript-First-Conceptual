161d3447ea1db6751bcad3c72a1f4998c3562925
Sheikh Jabed <jabedbd2295@gmail.com>
2024-08-31 23:23:52 +0600

    Some Lines and Operations Updated

--- math.js
+++ math.js
@@ -1,26 +1,10 @@
 // Addition Operation
 
-var x = 5;
-var y = 10;
-var z = 8.5;
+// var x = 5;
+// var y = 10;
+// var z = 8.5;
 
-console.log(x + y + z);
-
-var x = 5;
-var y = 10;
-var z = 8.5;
-var total = x + y + z;
-
-console.log(total);
-
-
-// Addition Operation
-
-var x = 5;
-var y = 10;
-var z = 8.5;
-
-console.log(x + y + z);
+// console.log(x + y + z);
 
 var x = 5;
 var y = 10;
@@ -30,39 +14,37 @@ var total = x + y + z;
 console.log(total);
 
 
-// Addition Operation
+// Subtraction Operation
+
+var x = 15;
+var y = 30;
+var z = 65;
+var sub = z - y -x;
+
+console.log(sub);
+
+
+// Multiplication Operation
 
 var x = 5;
 var y = 10;
-var z = 8.5;
+var z = 15;
+var multi = x * y * z;
 
-console.log(x + y + z);
+console.log(multi);
 
-var x = 5;
+
+// Division Operation
+
+var x = 550;
 var y = 10;
-var z = 8.5;
-var total = x + y + z;
+var z = 5;
+var divide = x / y / z;
 
-console.log(total);
-
-
-// Addition Operation
-
-var x = 5;
-var y = 10;
-var z = 8.5;
-
-console.log(x + y + z);
-
-var x = 5;
-var y = 10;
-var z = 8.5;
-var total = x + y + z;
-
-console.log(total);
+console.log(divide);
 
 
-// Addition Operation
+// Multi Operation
 
 var x = 5;
 var y = 10;
@@ -72,4 +54,24 @@ var sub = y - x;
 var multi = x * y;
 var divide = y / x;
 
-console.log(total);
+console.log(add);
+console.log(sub);
+console.log(multi);
+console.log(divide);
+
+
+// toFixed 
+
+var x = 105;
+var y = 9;
+var divide = x / y;
+var value = divide.toFixed(4);
+
+console.log(divide);
+console.log(value);
+
+console.log(typeof divide);
+console.log(typeof value);
+
+// parseFloat
+
